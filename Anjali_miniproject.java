48067cc697f01fe51d19ab02b4d055d1005e1472
Anjali Srivastava <anjalisriva95@gmail.com>
2017-07-11 00:21:31 +0530

    Adding Author's details

---
+++ Anjali_miniproject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anjali_miniproject;
+
+/**
+ *
+ * @author ANJALI
+ */
+public class Anjali_miniproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
